99d6e0681c585761686b953cd33367596fd68626
AlbertoDiazEsteban <albertodiazesteban@gmail.com>
2023-04-04 00:24:56 +0200

    actualizaciones

--- about.js
+++ about.js
@@ -44,50 +44,11 @@ animate();
 
 
 
-const me = document.querySelector('.me');
-const moons = document.querySelector('.moons');
-
-window.addEventListener('scroll', () => {
-  const scrollPosition = window.scrollY;
-
-  me.style.transform = `translateY(${scrollPosition * 0.5}px)`;
-  moons.style.transform = `translateY(${scrollPosition * 0.3}px)`;
-});
-
-
-/* efecto parallax sirena mar */
-const oceanLayer = document.querySelector('.sea-1');
-const mermaidLayer = document.querySelector('.sea-2');
-const bubblesLayer = document.querySelector('.sea-3');
-
-// mover las capas de la sirena y las burbujas hacia arriba
-mermaidLayer.style.transform = "translateY(-50%)";
-bubblesLayer.style.transform = "translateY(-100%)";
-
-window.addEventListener('scroll', () => {
-  const scrollPosition = window.pageYOffset;
-
-  oceanLayer.style.transform = `translateY(calc(-20% + ${scrollPosition * 0.2}px))`;
-  mermaidLayer.style.transform = `translateY(calc(-50% + ${scrollPosition * 0.4}px))`;
-  bubblesLayer.style.transform = `translateY(calc(-50% + ${scrollPosition * 0.4}px))`;
-});
 
 
 
 
-// const romantic2 = document.querySelector('.romantic-2');
 
-// const factor1 = 0.31;
-// const factor2 = 0.35;
-// const factor3 = 0.32;
-
-// window.addEventListener('scroll', function() {
-//   const scrollAmount = window.pageYOffset;
-  
-//   romantic1.style.transform = `translateY(${scrollAmount * factor1}px)`;
-//   romantic2.style.transform = `translateY(${scrollAmount * factor2}px)`;
-//   romantic3.style.transform = `translateY(${scrollAmount * factor3}px)`;
-// });
 
 const typingTextElement = document.getElementById("typing-text");
 const textArray = ["Graphic Designer", "Illustrator", "Web Developer", "AI lover", "many things..."];
